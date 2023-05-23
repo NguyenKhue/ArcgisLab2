5f7f0385d2b313eed78a1735bd065fcf61960613
NguyenKhue <nguyenkhue09032001@gmail.com>
2023-05-24 05:09:31 +0700

    nền nhà tòa phải

--- data/rightBuilding/floor/right.building.floor1.js
+++ data/rightBuilding/floor/right.building.floor1.js
@@ -1,33 +1,54 @@
 export const right_building_floor1 = (
-    Map,
-    SceneView,
-    GeoJSONLayer,
-    SceneLayer,
-    GraphicsLayer,
-    Graphic,
-    esriRequest
-  ) => {
-    const bottomFloor = new GeoJSONLayer({
-      url: "./data/rightBuilding/floor/right.building.bottom.floor.geojson.json",
-    });
+  Map,
+  SceneView,
+  GeoJSONLayer,
+  SceneLayer,
+  GraphicsLayer,
+  Graphic,
+  esriRequest
+) => {
+  const bottomFloor = new GeoJSONLayer({
+    url: "./data/rightBuilding/floor/right.building.bottom.floor.geojson.json",
+  });
 
-    bottomFloor.renderer = {
-        type: "simple",
-        symbol: {
-          type: "polygon-3d",
-          symbolLayers: [
-            {
-              type: "extrude",
-              size: 0.1,
-              material: {
-                color: "#8e8e8e",
-              },
-            },
-          ],
+  const floor = new GeoJSONLayer({
+    url: "./data/rightBuilding/floor/right.building.floor.geojson.json",
+  });
+
+  bottomFloor.renderer = {
+    type: "simple",
+    symbol: {
+      type: "polygon-3d",
+      symbolLayers: [
+        {
+          type: "extrude",
+          size: 0.1,
+          material: {
+            color: "#8e8e8e",
+          },
         },
-      };
+      ],
+    },
+  };
 
-      return [
-        bottomFloor
-      ]
+  floor.renderer = {
+    type: "simple",
+    symbol: {
+      type: "polygon-3d",
+      symbolLayers: [
+        {
+          type: "extrude",
+          size: 1.2,
+          material: {
+            color: "#64646c",
+          },
+        },
+      ],
+    },
+  };
+
+  return [
+    bottomFloor,
+    floor
+  ]
 }

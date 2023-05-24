a3ab5735ca18ef89cd4029413ebb8ebd75a3a8bd
NguyenTraVi <19522512@gm.uit.edu.vn>
2023-05-24 18:04:05 +0700

    done gate back left and right

--- data/floor/floor.js
+++ data/floor/floor.js
@@ -4,6 +4,8 @@ import { right_entry } from "./doors/right_entry/right_entry.js";
 import { left_step } from "./step/left_step/left_step.js";
 import {gate_left_wall} from "./doors/gate_left/gate_left.js"
 import {gate_right_wall} from "./doors/gate_right/gate_right.js"
+import {gate_back_right_wall} from "./doors/gate_back_right/gate_back_right.js"
+import {gate_back_left_wall} from "./doors/gate_back_left/gate_back_left.js"
 import { right_step } from "./step/right_step/right_step.js";
 import { floor_walls } from "./walls/foor_walls.js";
 import { floor_rails } from "./rails/floor_rails.js";
@@ -20,6 +22,8 @@ export const floor = (Map, SceneView, GeoJSONLayer, SceneLayer,
   const rails = floor_rails(Map, SceneView, GeoJSONLayer, SceneLayer, GraphicsLayer, Graphic, esriRequest)
   const gateLeftWall = gate_left_wall(Map, SceneView, GeoJSONLayer, SceneLayer, GraphicsLayer, Graphic, esriRequest)
   const gateRightWall = gate_right_wall(Map, SceneView, GeoJSONLayer, SceneLayer, GraphicsLayer, Graphic, esriRequest)
+  const gateBackRightWall = gate_back_right_wall(Map, SceneView, GeoJSONLayer, SceneLayer, GraphicsLayer, Graphic, esriRequest)
+  const gateBackLeftWall = gate_back_left_wall(Map, SceneView, GeoJSONLayer, SceneLayer, GraphicsLayer, Graphic, esriRequest)
 
   return [
     ...centerEntry,
@@ -30,6 +34,8 @@ export const floor = (Map, SceneView, GeoJSONLayer, SceneLayer,
     ...walls,
     ...rails,
     ...gateLeftWall,
-    ...gateRightWall
+    ...gateRightWall,
+    ...gateBackRightWall,
+    ...gateBackLeftWall
   ]
 }

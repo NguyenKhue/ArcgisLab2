e9fa10ba817fd99d7e9d5c5d72e38d3e69449819
Tien Dat <gachip9090@gmail.com>
2023-05-25 17:05:56 +0700

    update lan can tang 2

--- data/leftBuilding/secondFloor/rails/rail.js
+++ data/leftBuilding/secondFloor/rails/rail.js
@@ -19,9 +19,25 @@ export const wall_rail = (
     url: "./data/leftBuilding/secondFloor/rails/right.wall.rail.part2.geojson.json",
   });
 
-  //   const frontWallRail = new GeoJSONLayer({
-  //     url: "./data/leftBuilding/secondFloor/rails/front.wall.rail.geojson.json",
-  //   });
+  const leftWallRail = new GeoJSONLayer({
+    url: "./data/leftBuilding/secondFloor/rails/left.wall.rail.geojson.json",
+  });
+
+  const leftWallRail2 = new GeoJSONLayer({
+    url: "./data/leftBuilding/secondFloor/rails/left.wall.rail.part2.geojson.json",
+  });
+
+  const leftWallRail3 = new GeoJSONLayer({
+    url: "./data/leftBuilding/secondFloor/rails/left.wall.rail.part3.geojson.json",
+  });
+
+  const backWallRail = new GeoJSONLayer({
+    url: "./data/leftBuilding/secondFloor/rails/back.wall.rail.geojson.json",
+  });
+
+  const backWallRail2 = new GeoJSONLayer({
+    url: "./data/leftBuilding/secondFloor/rails/back.wall.rail.part2.geojson.json",
+  });
 
   frontWallRail.renderer = {
     type: "simple",
@@ -71,21 +87,94 @@ export const wall_rail = (
     },
   };
 
-  //   frontWallRail.renderer = {
-  //     type: "simple",
-  //     symbol: {
-  //       type: "polygon-3d",
-  //       symbolLayers: [
-  //         {
-  //           type: "extrude",
-  //           size: 0.8,
-  //           material: {
-  //             color: "#d8aa5e",
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   };
+  leftWallRail.renderer = {
+    type: "simple",
+    symbol: {
+      type: "polygon-3d",
+      symbolLayers: [
+        {
+          type: "extrude",
+          size: 0.6,
+          material: {
+            color: "#d8aa5e",
+          },
+        },
+      ],
+    },
+  };
 
-  return [frontWallRail, rightWallRail, rightWallRail2];
+  leftWallRail2.renderer = {
+    type: "simple",
+    symbol: {
+      type: "polygon-3d",
+      symbolLayers: [
+        {
+          type: "extrude",
+          size: 0.6,
+          material: {
+            color: "#d8aa5e",
+          },
+        },
+      ],
+    },
+  };
+
+  leftWallRail3.renderer = {
+    type: "simple",
+    symbol: {
+      type: "polygon-3d",
+      symbolLayers: [
+        {
+          type: "extrude",
+          size: 0.6,
+          material: {
+            color: "#d8aa5e",
+          },
+        },
+      ],
+    },
+  };
+
+  backWallRail.renderer = {
+    type: "simple",
+    symbol: {
+      type: "polygon-3d",
+      symbolLayers: [
+        {
+          type: "extrude",
+          size: 0.6,
+          material: {
+            color: "#d8aa5e",
+          },
+        },
+      ],
+    },
+  };
+
+  backWallRail2.renderer = {
+    type: "simple",
+    symbol: {
+      type: "polygon-3d",
+      symbolLayers: [
+        {
+          type: "extrude",
+          size: 0.6,
+          material: {
+            color: "#d8aa5e",
+          },
+        },
+      ],
+    },
+  };
+
+  return [
+    frontWallRail,
+    rightWallRail,
+    rightWallRail2,
+    leftWallRail,
+    leftWallRail2,
+    leftWallRail3,
+    backWallRail,
+    backWallRail2,
+  ];
 };
